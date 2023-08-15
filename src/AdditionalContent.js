b43176464fa0356ce74dca3d83bcec2aac244c43
Tom_Court <tomcourt94@icloud.com>
2023-08-15 11:07:07 +0100

    additional content template and about section

--- src/AdditionalContent.js
+++ src/AdditionalContent.js
@@ -2,7 +2,7 @@ export default function AdditionalContent() {
   return (
     <div className="AdditionalContent">
       <h1>Dad Joke Depot: Additional Content</h1>
-      <p>To be confirmed. Either more jokes or quotes or advice</p>
+      <p>If you would like to see more of my work, please visit my portfolio here: </p>
     </div>
   );
 }
